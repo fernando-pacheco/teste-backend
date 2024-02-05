66b7dbd38f5dee86f4dce7319b7abd282a194041
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2024-02-05 20:41:10 -0300

    criando a rota para o turnover

--- setup/urls.py
+++ setup/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from headcount.views import HeadcountViewSet
+from turnover.views import TurnoverViewSet
 
 router = routers.DefaultRouter()
 router.register('headcount', HeadcountViewSet, basename='headcount')
+router.register('turnover', TurnoverViewSet, basename='turnover')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
